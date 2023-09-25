0bcc80b106a24b77f5290d1faca5172003de9ed4
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-09-25 17:33:14 +0100

    created the product migration table

---
+++ database/migrations/2023_09_25_160303_create_products_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description', 1000);
+            $table->float('price')->unsigned();
+            $table->string('stock')->unsigned();
+            $table->string('status')->default('Unavailable');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
